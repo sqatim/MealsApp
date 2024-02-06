9b26c5206fefde90618e4caaac80f185674cec3d
Samir Qatim <sqatim@student.1337.ma>
2024-02-06 16:54:48 +0100

    feat: start project

---
+++ lib/screens/categories.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:meals/data/dummy_data.dart';
+import 'package:meals/widgets/category_grid_item.dart';
+
+class CategoriesScreen extends StatelessWidget {
+  const CategoriesScreen({super.key});
+
+  // void _selectCategory() {
+  //   Navigator.push(context, )
+  // }
+
+  @override
+  Widget build(context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Pick your category"),
+      ),
+      body: GridView(
+        padding: const EdgeInsets.all(24),
+        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 2,
+          childAspectRatio: 3 / 2,
+          mainAxisSpacing: 20,
+          crossAxisSpacing: 20,
+        ),
+        children: [
+          for (final category in availableCategories)
+            CategoryGridItem(category: category)
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/widgets/category_grid_item.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+import 'package:meals/data/dummy_data.dart';
+import 'package:meals/models/category.dart';
+import 'package:meals/screens/meals.dart';
+
+class CategoryGridItem extends StatelessWidget {
+  const CategoryGridItem({super.key, required this.category});
+
+  final Category category;
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+              builder: (context) => MealsScreen(
+                    title: category.title,
+                    meals: dummyMeals
+                        .where((meal) => meal.categories.contains(category.id))
+                        .toList(),
+                  )),
+        );
+      },
+      splashColor: Theme.of(context).primaryColor,
+      borderRadius: BorderRadius.circular(16),
+      child: Container(
+        padding: const EdgeInsets.all(16),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(16),
+          gradient: LinearGradient(
+            colors: [
+              category.color.withOpacity(0.55),
+              category.color.withOpacity(0.9),
+            ],
+          ),
+        ),
+        child: Text(
+          category.title,
+          style: Theme.of(context)
+              .textTheme
+              .titleLarge!
+              .copyWith(color: Theme.of(context).colorScheme.onBackground),
+        ),
+      ),
+    );
+  }
+}
